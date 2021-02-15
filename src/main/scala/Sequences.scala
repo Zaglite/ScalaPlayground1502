909bd91c156d5d9cf5fe5b2bb45eb3bf85ecb8f8
Zaglite <eglites@gmail.com>
2021-02-15 23:32:40 +0200

    Immutable Sequences, map filter

--- src/main/scala/Sequences.scala
+++ src/main/scala/Sequences.scala
@@ -29,5 +29,38 @@ object Sequences extends App {
   println(filteredSeq)
   mySeq.foreach(el => println(el+20)) //thus will print each element +20
 
+  //Scala is not keen on mutating, but it is possible
+  //generally it is done before finalizing data into some immutable
+  //https://alvinalexander.com/scala/how-to-append-prepend-items-vector-seq-in-scala/
+  println(myMutableSeq :+ 100)
+  println(myMutableSeq) //it prints, but it is nod saved
+  val newMutable = myMutableSeq :+ 100 //thus is how we save data
+  println(newMutable)
+  val newImmutable = mySeq :+ 2000 //so again it has to be saved
+  println(newImmutable)
+  newMutable.drop(6)
+  println(newMutable)
+  val anotherMutable = newMutable.drop(2) //it drops from beginning
+  println(anotherMutable)
 
+  val bigSeq = (1 to 10).toVector //range is smth half ready given on demand, VECTOR is smth already made
+  println(bigSeq)
+  println(bigSeq.slice(3,6)) //prints from till, 4 is 3rd element, 7 (not included) is 6th element
+  val newSlice = bigSeq.slice(2,5) //SLICING and DICING
+  println(newSlice)
+  println(newSlice.length)
+  val by1000 = newSlice.map(_ * 1000)
+  println(by1000)
+  //usually Seq and Scala pick the most appropriate data container
+
+  //Array comes fom Java and is not as nice as printing
+  val a2 = Array("one", "two", "three", "four")
+  val a3 = Array(100, "two", "three", "four") //try to avoid this! keep one type per sequence
+  println(by1000(2))
+  println(by1000.last)
+  println(a2.slice(1,3).toSeq.toString)
+
+  println(bigSeq.sum)
+  println(bigSeq.length)
+  println(bigSeq.sum/bigSeq.length. toDouble) //aritmetic means one of the values be DOUBLE long as regular
 }
